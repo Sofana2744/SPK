67e52effa4ad71ed394a30075ccea3888d4bad23
Riobush32 <riobush32@gmail.com>
2024-05-29 00:01:00 +0700

    function update criteria clear

---
+++ app/Http/Controllers/CriteriaController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Criteria;
+use Illuminate\Http\Request;
+
+class CriteriaController extends Controller
+{
+    public function index(){
+        return view("pages.criteria.index",
+        [
+            'data' => Criteria::all(),
+            "title" => "Criteria"
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        Criteria::create([
+            'name' => $request->criteria,
+        ]);
+
+        return back();
+    }
+
+    public function update(Request $request, $id)
+    {
+        $criteria = Criteria::find($id);
+        $criteria->update([
+            'name' => $request->criteria, 
+        ]);
+        return back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CriteriaController;
+
+Route::get('/criteria', [CriteriaController::class,'index'] );
+Route::post('/criteria', [CriteriaController::class,'store'] );
+Route::patch('/criteria/{id}', [CriteriaController::class, 'update'] );
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
